353b60454ec9d76303f19a39d1503f960dfa51d8
SeniorManTv1 <143917011+SeniorManTv1@users.noreply.github.com>
2023-09-18 23:44:07 +0100

    Update config.js

--- config.js
+++ config.js
@@ -17,10 +17,10 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["8801853262586","8801928502256"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["8801853262586","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.ownertag = ["8801928502256"];
-global.OwnerName = "Slasher";
+global.Owner = ["09034009884","09034009884"];         //If you want singal number so global.Owner = ['09034009884'] Change into your number.
+global.OwnerNumber = ["09034009884","09034009884"];   //If you want singal number so global.Owner = ['09034009884'] Change into your number.
+global.ownertag = ["09034009884"];
+global.OwnerName = "SeniorManTv";
 global.BotName = "XLICON-MD";
 global.packname = "X-2.0 Bot";                             //Do not change.
 global.author = "By: Slasher";                               //Do not change.
@@ -33,12 +33,12 @@ global.sessionName = "session";                          //Do not change.
 
 
 //
-global.prefa = ["."];                                    //Default prefix here.
+global.prefa = ["⚡"];                                    //Default prefix here.
 global.openAiAPI = "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";
 
 
 //
-global.location = "Dhaka, Bangladesh";                   
+global.location = "Kwara, Nigeria";                   
 global.reactmoji = "✅";
 global.themeemoji = "💖";
 global.vidmenu = { url: 'https://tenor.com/view/jujutsu-kaisen0-yuta-okkotsu-gif-26767662' };
